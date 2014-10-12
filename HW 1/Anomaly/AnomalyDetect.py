63840c7427b39db4df3f8ada2f4847f6e362655b
Ankur Bapna <ankur.bpn@gmial.com>
2014-10-12 00:46:07 -0700

    Read and center training data

--- HW 1/Anomaly/AnomalyDetect.py
+++ HW 1/Anomaly/AnomalyDetect.py
@@ -11,7 +11,10 @@ NUM_SEQUENCES = 0
 LENGTH_SEQUENCES = 100 
 
 #Total number of sequences for training
-TOTAL_SEQUENCES = 5000*50/LENGTH_SEQUENCES
+TOTAL_TRAINING_SEQUENCES = 5000*50/LENGTH_SEQUENCES
+
+#Training sequences per user
+TRAINING_SEQ = 5000/LENGTH_SEQUENCES
 
 #Window size for co-occurence calculation/Scope
 #Curently we assign 1 for each co-occurence within window irrespective of distance 
@@ -21,7 +24,11 @@ SCOPE = 6
 commandToIndex = {}
 
 #Data matrix for storing training sequences in row major form
-trainingData = csr_matrix((TOTAL_SEQUENCES, M*M), dtype = float)
+trainingData = csr_matrix((TOTAL_TRAINING_SEQUENCES, M*M), dtype = float)
+
+#Data vector for storing mean of the training sequences for centering
+#meanData = csr_matrix((1, M*M), dtype = float)
+#numVals = csr_matrix((1, M*M), dtype = float)
 
 #Function to generate a co-occurence matrix vector of length M*M from command sequence
 def generateSequenceVector(sequence):
@@ -46,7 +53,7 @@ def generateSequenceVector(sequence):
 
 
 
-#To read through all the files for the 50 users and generate co-occurence matrices for the 50 users
+#To read through all the files for the 50 users and generate co-occurence matrices for training sequences for the 50 users
 for i in range(50):
 	userNo = i+1
 	#print "User %d" % userNo
@@ -55,9 +62,12 @@ for i in range(50):
 	counter = 0
 	#List to divide User data into sequences
 	commandList = []
+	seqNo = 0
 
 	for line in file:
 		counter = counter+1
+		if counter > 5000:
+			break
 		commandList.append(line)
 		if not commandToIndex.has_key(line):
 			commandToIndex[line] = m
@@ -67,7 +77,21 @@ for i in range(50):
 		if counter%LENGTH_SEQUENCES == 0:
 			y, data = generateSequenceVector(commandList)
 			commandList = []
+			x = [seqNo + i*TRAINING_SEQ]*len(y)
+			seqNo = seqNo + 1
+			trainingData = trainingData + csr_matrix((data, (x, y)), shape = (TOTAL_TRAINING_SEQUENCES, M*M))
+			#meanData = meanData + csr_matrix((data, ([0]*len(y), y)), shape = (1, M*M))
+			#numVals = numVals + csr_matrix(([1]*len(y), ([0]*len(y), y)), shape = (1, M*M))
 
-		#if 
+
+	if len(commandList) > 0:
+		y, data = generateSequenceVector(commandList)
+		commandList = []
+		trainingData = trainingData + csr_matrix((data, (x, y)), shape = (TOTAL_TRAINING_SEQUENCES, M*M))
+		#meanData = meanData + csr_matrix((data, ([0]*len(y), y)), shape = (1, M*M))
+		#numVals = numVals + csr_matrix(([1]*len(y), ([0]*len(y), y)), shape = (1, M*M))
  
-
+#Centering training data
+meanData = trainingData.mean(axis = 1)
+trainingDataCentered = trainingData - (meanData*([1]*TOTAL_TRAINING_SEQUENCES).T).T
+del trainingData
