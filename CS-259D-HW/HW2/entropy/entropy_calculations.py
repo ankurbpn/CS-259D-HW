a21707a93758fe3026a3d62c35958bafcea4fd26
Ankur Bapna <ankurbpn@github.com>
2014-11-01 11:38:14 -0700

    Entropy calc completed

--- CS-259D-HW/HW2/entropy/entropy_calculations.py
+++ CS-259D-HW/HW2/entropy/entropy_calculations.py
@@ -1,5 +1,6 @@
-##from matplotlib import pyplot
+import matplotlib.pyplot as plt
 import math
+from datetime import datetime
 
 def read_data_from_file():
 	data = []
@@ -11,18 +12,21 @@ def read_data_from_file():
 def get_entropy_from_dict(dic, total):
 	H = 0
 	for key, val in dic.iteritems():
-		H -= (1.0*val/total)*math.log(val/total)
+		H -= (1.0*val/total)*math.log(1.0*val/total)
 	return H
 
 def get_all_entropy_lists():
-	lis_features = [3, 4, 5, 6, 7, 8]
+	#lis_features = [3, 4, 5, 6, 7, 8]
+	lis_features = [3]
 	pairwise_ent = {}
 	ent = {}
 	pairwise_count = {}
 	count = {}
 	dat = read_data_from_file()
+	tim = []
 	total = 0
 	for item in dat:
+		tim.append(datetime.strptime(item[2], '%H:%M:%S'))
 		total += 1
 		for i in lis_features:
 			if i in count.keys():
@@ -41,21 +45,47 @@ def get_all_entropy_lists():
 
 			for j in lis_features:
 				if i!=j:
-				if (i, j) in pairwise_count.keys():
-					if (item[i], item[j]) in pairwise_count[(i, j)].keys():
-						pairwise_count[(i, j)][(item[i], item[j])]+=1
+					if (i, j) in pairwise_count.keys():
+						if (item[i], item[j]) in pairwise_count[(i, j)].keys():
+							pairwise_count[(i, j)][(item[i], item[j])]+=1
+						else:
+							pairwise_count[(i, j)][(item[i], item[j])]=1
 					else:
-						pairwise_count[(i, j)][(item[i], item[j])]=1
-				else:
-					pairwise_count[(i, j)] = {}
-					pairwise_count[(i, j)][(item[i], item[j])]=1				
+						pairwise_count[(i, j)] = {}
+						pairwise_count[(i, j)][(item[i], item[j])]=1				
 
-				if (i, j) in pairwise_ent.keys():
-					pairwise_ent[(i, j)].append(get_entropy_from_dict(pairwise_count[(i, j)], total))
-				else:
-					pairwise_ent[(i, j)] = [get_entropy_from_dict(pairwise_count[(i, j)], total)]
-		print total
+					if (i, j) in pairwise_ent.keys():
+						pairwise_ent[(i, j)].append(get_entropy_from_dict(pairwise_count[(i, j)], total))
+					else:
+						pairwise_ent[(i, j)] = [get_entropy_from_dict(pairwise_count[(i, j)], total)]
+		print tim[total-1]
+	return tim, ent, pairwise_ent
+
+def get_feature_name():
+	dict = {}
+	dict[3] = 'Duration'
+	dict[4] = 'Serv'
+	dict[5] = 'Src port'
+	dict[6] = 'Dest Port'
+	dict[7] = 'Src IP'
+	dict [8] = 'Dest IP'
+	return dict
+
+def plot_all_entropy_lists():
+	time, ent, pairwise_ent = get_all_entropy_lists()
+	feat = get_feature_name()
+	for i in ent.keys():
+		plt.xlabel('Time')
+		plt.ylabel('entropy ' + feat[i] )
+		plt.plot(time, ent[i])
+		plt.show()
+	for i in pairwise_ent.keys():
+		plt.xlabel('Time')
+		plt.ylabel('entropy ' + feat[i[0]] + ' + ' + feat[i[1]])
+		plt.plot(time, pairwise_ent[i])
+		plt.show()
+
 
 				
-get_all_entropy_lists()
+plot_all_entropy_lists()
 #read_data_from_file()
